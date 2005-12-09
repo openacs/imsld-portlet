c0229a9ce10051a7750fd11eb88f7012efad62da
Jose Pablo Escobedo Del Cid <jose.escobedo@int-edu.eu>
2005-12-09 12:01:01 +0000

    fixing bug when installing in more than one community

--- www/imsld-portlet.tcl
+++ www/imsld-portlet.tcl
@@ -19,14 +19,16 @@ set community_id [dotlrn_community::get_community_id]
 
 if { ![empty_string_p $community_id] } {
     # were are inside a community 
+    set cr_root_folder_id [imsld::cr::ger_root_folder -community_id $community_id]
+
     db_multirow imslds_in_class get_manifests {
         select cr3.item_id as imsld_id,
         coalesce(imsld.title, imsld.identifier) as imsld_title
-        from acs_rels ar, cr_items cr1, cr_items cr2, cr_items cr3, imsld_cp_manifests icm, imsld_cp_organizations ico, imsld_imsldsi imsld 
-        where ar.object_id_one = :community_id
-        and ar.rel_type = 'imsld_community_manifest_rel'
-        and ar.object_id_two = cr1.item_id
-        and cr1.live_revision = icm.manifest_id
+        from cr_items cr1, cr_items cr2, cr_items cr3, cr_items cr4,
+        imsld_cp_manifests icm, imsld_cp_organizations ico, imsld_imsldsi imsld 
+        where cr1.live_revision = icm.manifest_id
+        and cr1.parent_id = cr4.item_id
+        and cr4.parent_id = :cr_root_folder_id
         and ico.manifest_id = cr1.item_id
         and imsld.organization_id = cr2.item_id
         and cr2.live_revision = ico.organization_id
