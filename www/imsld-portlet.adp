08142cf44664afedc032fe63c69d3d25b16959f4
Jose Pablo Escobedo Del Cid <jose.escobedo@int-edu.eu>
2006-04-19 16:31:05 +0000

    removing portlet table. replacing it with links to the new imsld-tree (using a lib file in the imsld package)

--- www/imsld-portlet.adp
+++ www/imsld-portlet.adp
@@ -1,18 +1,7 @@
 <if @shaded_p@ false>
 
 <if @community_id@ not nil>
- <if @imslds_in_class:rowcount@ gt 0>
-  <blockquote>
-  <multiple name="imslds_in_class">
-  <li><strong>@imslds_in_class.imsld_title@</strong><br />
-      <include src="../../imsld/lib/imsld-chunk" imsld_item_id="@imslds_in_class.imsld_id@"> 
-  <br /><br />
-  </multiple>
-  </blockquote>
- </if>
- <else>
-  #imsld-portlet.There_are_no_units-of-learning_for_this_course#.
- </else>
+	<include src="../../imsld/lib/imsld-chunk" community_id=@community_id@ orderby=@orderby@>
  </if>
 <else>
 not implemented
--- www/imsld-portlet.tcl
+++ www/imsld-portlet.tcl
@@ -6,7 +6,8 @@ ad_page_contract {
     @creation-date 2005-09-27
     @cvs-id $Id$
 
-} -query {
+} {
+    {orderby ""}
 }
 
 array set config $cf
@@ -16,27 +17,3 @@ set list_of_package_ids [lsort $config(package_id)]
 set one_instance_p [ad_decode [llength $list_of_package_ids] 1 1 0]
 
 set community_id [dotlrn_community::get_community_id]
-
-if { ![empty_string_p $community_id] } {
-    # were are inside a community 
-    set cr_root_folder_id [imsld::cr::get_root_folder -community_id $community_id]
-
-    db_multirow imslds_in_class get_manifests {
-        select cr3.item_id as imsld_id,
-        coalesce(imsld.title, imsld.identifier) as imsld_title
-        from cr_items cr1, cr_items cr2, cr_items cr3, cr_items cr4,
-        imsld_cp_manifests icm, imsld_cp_organizations ico, imsld_imsldsi imsld 
-        where cr1.live_revision = icm.manifest_id
-        and cr1.parent_id = cr4.item_id
-        and cr4.parent_id = :cr_root_folder_id
-        and ico.manifest_id = cr1.item_id
-        and imsld.organization_id = cr2.item_id
-        and cr2.live_revision = ico.organization_id
-        and cr3.live_revision = imsld.imsld_id
-    } {
-    }
-} else {
-    # not implemented!
-}
-
-ad_return_template 
